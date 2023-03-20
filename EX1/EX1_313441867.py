ab6dbebf205666640cc72c0d87204b47f2389bc7
Husen Abdallah <Husen.ab1015@gmail.com>
2023-03-20 15:23:56 +0200

    fixed function valid_args to deal with signed integers instead

--- EX1/EX1_313441867.py
+++ EX1/EX1_313441867.py
@@ -2,16 +2,51 @@ import sys
 
 # command line arguments
 file_name = sys.argv[1]
-
 # file_name = "in.csv"
+
+
+def valid_args(line_args: list):
+    if len(line_args) == 3:
+        first = line_args[0]
+        second = int(line_args[1])
+        third = line_args[2]
+        # validate first & second arg
+
+        # if 3rd arg is string
+        if first == '1' and second == len(third):
+            return True
+
+        # if 3rd arg is int make sure 2nd argument = how many bytes needed for 3rd argument
+        if (second != 0) and (second & (second-1) == 0):  # check if second arg is power of 2
+            binary_form = bin(int(third))  # 3rd arg in binary
+            negative = False
+            if binary_form[0] == '-':
+                negative = True
+            binary_form = binary_form[3:] if negative else binary_form[2:]  # remove - from negative numbers
+            bytes_num = (len(binary_form) + 1) / 8  # how many bytes to represent the 3rd arg,
+            # + 1 to account for the signed bit
+
+            # here im basically doing a ceil(bytes_num) manually since I cant import math
+            if not float(bytes_num).is_integer():
+                bytes_num = int(bytes_num) + 1
+            else:
+                bytes_num = int(bytes_num)
+
+            if first == '2' and second == bytes_num:
+                return True
+
+    return False
+
+
 try:
     with open(file_name, "r") as csv_file:
         # Find the maximum length of the third parameter.
         max_len = 0
         for line in csv_file:
             args = line.strip().split(",")
-            if len(args) != 3:
-                raise ValueError("bad CSV file format")
+
+            if not valid_args(args):
+                raise ValueError("bad CSV file format or illegal values")
             max_len = max(max_len, len(args[2]))
 
         # Print the table headers.
@@ -34,4 +69,3 @@ except IOError:
 except ValueError as err:
     print(err)
     sys.exit(1)
-
