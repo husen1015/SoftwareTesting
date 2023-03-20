35b248dd69e82e6670f78c6f6c103282c0ce4c85
Husen Abdallah <Husen.ab1015@gmail.com>
2023-03-20 13:04:59 +0200

    added some error catching and code optimizing

--- EX1/EX1_313441867.py
+++ EX1/EX1_313441867.py
@@ -1,38 +1,37 @@
 import sys
 
-# arguments
-# type = sys.argv[0]
-# length = sys.argv[1]
-# value = sys.argv[2]
-import sys
+# command line arguments
+file_name = sys.argv[1]
 
-max_len = 0
-# find the longest length for the 3rd parameter according to which set the size of the last column
+# file_name = "in.csv"
 try:
-    csv_file = open("in.csv", "r")  # opening it again for reading
+    with open(file_name, "r") as csv_file:
+        # Find the maximum length of the third parameter.
+        max_len = 0
+        for line in csv_file:
+            args = line.strip().split(",")
+            if len(args) != 3:
+                raise ValueError("bad CSV file format")
+            max_len = max(max_len, len(args[2]))
+
+        # Print the table headers.
+        print("|{:<7}| {:<6}| {:<{length}}|".format("Type", "Length", "Value", length=max_len))
+        print("|{:<7}| {:<6}| {:<{length}}|".format("-------", "------", "-" * max_len, length=max_len))
+
+        # Print the actual lines.
+        csv_file.seek(0)  # Go back to the beginning of the file.
+        for line in csv_file:
+            args = line.strip().split(",")
+            # if len(args) != 3:
+            #     raise ValueError("CSV file is not well-formed")
+            arg_type, length, csv_value = args
+            print("|{:<7}| {:<6}| {:<{length}}|".format(arg_type.strip(), length.strip(), csv_value.strip(),
+                                                        length=max_len))
+
 except IOError:
-    print("error opening file")
-    exit(1)
-for line in csv_file:
-    line = line.split(',')
-    line_len = int(line[1])
-    max_len = line_len if max_len < line_len else max_len
+    print("IOError raised: can't opening file")
+    sys.exit(1)
+except ValueError as err:
+    print(err)
+    sys.exit(1)
 
-
-# headers
-print("|{:<7}| {:<6}| {:<{length}}|".format('Type', 'Length', 'Value', length=max_len))
-print("|{:<7}| {:<6}| {:<{length}}|".format('-------', '------', '-' * max_len, length=max_len))
-
-try:
-    csv_file = open("in.csv", "r")  # opening it again for reading
-except IOError:
-    print("error opening file")
-    exit(1)
-# print actual lines
-for line in csv_file:
-    args = line.split(',')
-    arg_type = args[0].rstrip()  # ignore trailing characters
-    length = args[1].rstrip()
-    csv_file = args[2].rstrip()
-
-    print("|{:<7}| {:<6}| {:<{length}}|".format(arg_type, length, csv_file, length=max_len))
